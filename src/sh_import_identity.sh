f944557b15319c43e3b53022a42e5da4351ee8bb
pndr 211c <pndrc247@gmail.com>
2024-02-02 14:17:07 +0800

    update

---
+++ src/sh_import_identity.sh
@@ -0,0 +1,3 @@
+dfx identity import admin1 ./private_key.pem
+dfx identity use admin1
+dfx identity get-principal
---
+++ src/sh_init_function_call.sh
@@ -0,0 +1,1 @@
+dfx deploy --argument '(opt record { owner = principal "yhyxg-izacd-xurb2-is55r-xwn23-k5zls-zjb6d-jspck-ybwnw-pqjkt-nae" })' random_number
